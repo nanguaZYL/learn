3363b90cac112093509e9f930b1f7513ed8ff899
zyl <2319782710@qq.com>
2021-12-03 09:17:04 +0800

    vite创建vue2项目

--- vite-vue/README.md
+++ vite-vue/README.md
@@ -1,4 +1,8 @@
 # vite vue2
 
-先用 yarn create vite 创建 vite 项目
+先用 yarn create vite 创建 vite 项目  // npm可用npm init vite@latest
 
+
+# 跟vue-cli不同的是它创建的index.html不在public目录下而是在项目根目录下而且需要<script type="module" src="/src/main.js"></script>引入js
+# 在开发期间 Vite 是一个服务器，而 index.html 是该 Vite 项目的入口文件。
+# Vite 将 index.html 视为源码和模块图的一部分。Vite 解析 <script type="module" src="..."> ，这个标签指向你的 JavaScript 源码。甚至内联引入 JavaScript 的 <script type="module"> 和引用 CSS 的 <link href> 也能利用 Vite 特有的功能被解析。另外，index.html 中的 URL 将被自动转换，因此不再需要 %PUBLIC_URL% 占位符了。
--- vite-vue/vite.config.js
+++ vite-vue/vite.config.js
@@ -1,7 +1,31 @@
 import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+const { createVuePlugin } = require('vite-plugin-vue2')
+import { resolve } from 'path'
 
-// https://vitejs.dev/config/
+// vite.config.js
 export default defineConfig({
-  plugins: [vue()]
+	plugins: [
+		createVuePlugin({
+			vueTemplateOptions: {},
+		}),
+	],
+	resolve: {
+		extensions: ['.vue', '.mjs', '.js', '.ts', '.jsx', '.tsx', '.json'],
+		alias: {
+			// vue2项目别名一般都是@，vue3中一般使用/@/, 为方便使用
+			'@': resolve('src'),
+			'~@': resolve('src'),
+		},
+	},
+	server: {
+		port: '3000',
+		host: '0.0.0.0',
+		proxy: {
+			// "/rep":"https://jsonplaceholder.typicode.com"
+			'/rep': {
+				target: 'https://jsonplaceholder.typicode.com', // 凡是遇到 /api 路径的请求，都映射到 target 属性
+				changeOrigin: true,
+			},
+		},
+	},
 })
